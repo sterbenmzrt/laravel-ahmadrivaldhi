cdeb9c88720a2055328458564fb892015f82eb92
ahmad rivaldhi <ahmad.rivaldhi@sekolahmu.co.id>
2025-07-11 11:22:13 +0700

    revert boostrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,11 +15,4 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    });
-
-// TAMBAHKAN KODE INI UNTUK VERCEL
-if (isset($_ENV['VERCEL'])) {
-    $app->useStoragePath($_ENV['VERCEL_TMP_DIR'] . '/storage');
-}
-
-return $app;
+    })->create();
