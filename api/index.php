f709c6765b2d59b0f31074507eb208390c09d969
ahmad rivaldhi <ahmad.rivaldhi@sekolahmu.co.id>
2025-07-11 10:23:56 +0700

    prepare deploy vercell

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Arahkan semua request ke entry point utama Laravel.
+require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    });
+
+// TAMBAHKAN KODE INI UNTUK VERCEL
+if (isset($_ENV['VERCEL'])) {
+    $app->useStoragePath($_ENV['VERCEL_TMP_DIR'] . '/storage');
+}
+
+return $app;
